664a3fd2f24d4a1b0912d2353454dcfa98c581cb
Ziappo <edo.piazzolla@stud.uniroma3.it>
2024-04-23 15:42:38 +0200

    primi modelli con attributi da completare

---
+++ siw-food/src/main/java/it/uniroma3/siw/model/Ricetta.java
@@ -0,0 +1,61 @@
+package it.uniroma3.siw.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Ricetta {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String nome;
+	
+	private List<String> urlImage;
+	
+	/* mettere la possibilità di scrivere tanto*/
+	private String descrizione;
+	
+	private Cuoco cuoco;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<String> getUrlImage() {
+		return urlImage;
+	}
+
+	public void setUrlImage(List<String> urlImage) {
+		this.urlImage = urlImage;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public Cuoco getCuoco() {
+		return cuoco;
+	}
+
+	public void setCuoco(Cuoco cuoco) {
+		this.cuoco = cuoco;
+	}
+
+
+	
+
+}
