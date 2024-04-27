e3d83a25f056f2a4e281acf9992cb52e376df61e
DESKTOP-PCOIPLV\EDOARDO <edo.piazzolla@stud.uniroma3.it>
2024-04-27 12:44:20 +0200

    miglioramenti visuali aggiunta ricette e ingreduienti

--- siw-food/src/main/java/it/uniroma3/siw/controller/RicettaController.java
+++ siw-food/src/main/java/it/uniroma3/siw/controller/RicettaController.java
@@ -1,8 +1,32 @@
 package it.uniroma3.siw.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import it.uniroma3.siw.repository.RicettaRepository;
+import it.uniroma3.siw.service.RicettaService;
 
 @Controller
 public class RicettaController {
+	
+	@Autowired RicettaRepository ricettaRepository;
+	
+	@Autowired RicettaService ricettaService;
+	
+	
+	@GetMapping("/ricetta/{id}")
+	public String getRicetta(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("ricetta", this.ricettaRepository.findById(id).get());
+		return "ricetta.html";
+	}
 
+	@GetMapping("/ricette")
+	public String Showricetta(Model model) {
+		model.addAttribute("ricette", this.ricettaService.findAll());
+		return "ricette.html";
+	}
+	
 }
