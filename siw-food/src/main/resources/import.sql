2c144904c8570388164cae05c80e2a962716216a
Ziappo <edo.piazzolla@stud.uniroma3.it>
2024-05-09 14:55:10 +0200

    aggiunta immagini per inserimento

--- siw-food/src/main/resources/import.sql
+++ siw-food/src/main/resources/import.sql
@@ -4,7 +4,7 @@ insert into ricetta (id, nome, url_Image, descrizione) values(nextval('ricetta_s
 insert into ricetta (id, nome, url_Image, descrizione) values(nextval('ricetta_seq'), 'Cacio e Pepe', 'cacio_e_pepe.png', 'Un piatto semplice ma delizioso, la cacio e pepe è una pasta romana che mette in evidenza la bellezza dei sapori semplici. Preparata con solo tre ingredienti principali - pecorino romano, pepe nero e pasta - questa pietanza è un esempio classico della cucina italiana. Il pecorino romano viene fuso con un po  d acqua di cottura della pasta per creare una salsa cremosa e saporita, mentre il pepe nero aggiunge un tocco di calore e spezie. Servita con pasta spaghetto o tonnarelli, la cacio e pepe è un esperienza culinaria genuina e appagante.');
 
 insert into cuoco (id, nome, cognome, url_Image, nascita) values(nextval('cuoco_seq'), 'Bruno', 'Barbieri', 'BrunoBarbieri.png','1958-08-25');
-insert into cuoco (id, nome, cognome, url_Image, nascita) values(nextval('cuoco_seq'), 'Antonino', 'Cannavacciuolo', 'Antonio-Cannavacciuolo.png','1990-10-13');
+insert into cuoco (id, nome, cognome, url_Image, nascita) values(nextval('cuoco_seq'), 'Antonino', 'Cannavacciuolo', 'Antonino-Cannavacciuolo.png','1990-10-13');
 insert into cuoco (id, nome, cognome, url_Image, nascita) values(nextval('cuoco_seq'), 'Massimiliano', 'Mariola', 'max_mariola.png','1969-04-11');
 insert into cuoco (id, nome, cognome, url_Image, nascita) values(nextval('cuoco_seq'), 'Giorgio', 'Barchiesi', 'giorgione.png','1957-06-07');
 
