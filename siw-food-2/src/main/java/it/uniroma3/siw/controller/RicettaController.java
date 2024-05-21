aac4d7d5e636ac3137b5c2b2fb3b19588881f110
Ziappo <edo.piazzolla@stud.uniroma3.it>
2024-05-21 15:31:03 +0200

    risolta impleentazione che quando un cuoco inserisce una nuova ricetta prende subito il nome e il cognome del cuoco e inizio a inserire la logica per visualizzare le quantità degi ingredienti e anche l'aggiunta della quantità e della modifica della quantità

--- siw-food-2/src/main/java/it/uniroma3/siw/controller/RicettaController.java
+++ siw-food-2/src/main/java/it/uniroma3/siw/controller/RicettaController.java
@@ -1,11 +1,9 @@
 package it.uniroma3.siw.controller;
 
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -35,47 +33,52 @@ import jakarta.validation.Valid;
 
 @Controller
 public class RicettaController {
-	
-	@Autowired RicettaRepository ricettaRepository;
-	
-	@Autowired RicettaService ricettaService;
-	
-	@Autowired RicettaValidator ricettaValidator;
-	
-	@Autowired CuocoService cuocoService;
-	
-	@Autowired CuocoRepository cuocoRepository;
-	
-	@Autowired IngredienteRepository ingredienteRepository;
-	
-	@Autowired CredentialsRepository credentialsRepository;
 
-	
-	
+	@Autowired
+	RicettaRepository ricettaRepository;
+
+	@Autowired
+	RicettaService ricettaService;
+
+	@Autowired
+	RicettaValidator ricettaValidator;
+
+	@Autowired
+	CuocoService cuocoService;
+
+	@Autowired
+	CuocoRepository cuocoRepository;
+
+	@Autowired
+	IngredienteRepository ingredienteRepository;
+
+	@Autowired
+	CredentialsRepository credentialsRepository;
+
 	@GetMapping("/ricetta/{id}")
 	public String getRicetta(@PathVariable("id") Long id, Model model) {
 		model.addAttribute("ricetta", this.ricettaRepository.findById(id).get());
 		return "ricetta.html";
 	}
-	
+
 	@GetMapping("/ricette")
 	public String ShowRicettaIndex(Model model) {
 		model.addAttribute("ricette", this.ricettaService.findAll());
 		return "ricette.html";
 	}
-	
+
 	@PostMapping("/searchRicette")
 	public String searchRicette(Model model, @RequestParam String nome) {
 		model.addAttribute("ricette", this.ricettaRepository.findByNome(nome));
 		return "ricette.html";
 	}
-	
+
 	@PostMapping("admin/searchRicette")
 	public String searchRicetteAdmin(Model model, @RequestParam String nome) {
 		model.addAttribute("ricette", this.ricettaRepository.findByNome(nome));
 		return "/admin/manageRicette.html";
 	}
-	
+
 	@PostMapping("cuoco/searchRicette")
 	public String searchRicetteCuoco(Model model, @RequestParam String nome) {
 		model.addAttribute("ricette", this.ricettaRepository.findByNome(nome));
@@ -87,21 +90,22 @@ public class RicettaController {
 		model.addAttribute("ricette", this.ricettaService.findAll());
 		return "/admin/manageRicette.html";
 	}
-	
+
 	@GetMapping("/cuoco/manageRicette")
 	public String ShowRicettaCuoco(Model model) {
 		model.addAttribute("ricette", this.ricettaService.findAll());
 		return "/cuoco/manageRicette.html";
 	}
-	
+
 	@GetMapping(value = "/admin/formNewRicetta")
 	public String formNewRicetta(Model model) {
 		model.addAttribute("ricetta", new Ricetta());
 		return "/admin/formNewRicetta.html";
 	}
-	
+
 	@PostMapping("/admin/ricetta")
-	public String newRicetta(@Valid @ModelAttribute("ricetta") Ricetta ricetta, BindingResult bindingResult, Model model) {
+	public String newRicetta(@Valid @ModelAttribute("ricetta") Ricetta ricetta, BindingResult bindingResult,
+			Model model) {
 		this.ricettaValidator.validate(ricetta, bindingResult);
 		if (!bindingResult.hasErrors()) {
 			this.ricettaRepository.save(ricetta);
@@ -111,37 +115,38 @@ public class RicettaController {
 			return "/admin/formNewRicetta.html";
 		}
 	}
-	
-	@GetMapping(value="/cuoco/formNewRicetta/{username}")
+
+	@GetMapping(value = "/cuoco/formNewRicetta/{username}")
 	public String formNewRicettaCuoco(@PathVariable("username") String username, Model model) {
-		Credentials tempUser = credentialsRepository.findByUsername(username);
+	    Credentials tempUser = credentialsRepository.findByUsername(username);
 	    User currentUser = tempUser.getUser();
 	    Cuoco currentCuoco = this.cuocoRepository.findByNomeAndCognome(currentUser.getNome(), currentUser.getCognome());
-		Ricetta ricetta = new Ricetta();
-		// Passa il cuoco al modello
+	    Ricetta ricetta = new Ricetta();
 	    model.addAttribute("cuoco", currentCuoco);
-		model.addAttribute("ricetta", ricetta);
-		return "cuoco/formNewRicetta.html";
+	    model.addAttribute("cuocoId", currentCuoco.getId());
+	    model.addAttribute("ricetta", ricetta);
+	    model.addAttribute("userDetails", tempUser); // Aggiungi userDetails al modello
+	    return "cuoco/formNewRicetta.html";
 	}
-	
-	@PostMapping("/cuoco/ricetta")
-	public String newRicettaCuoco(@Valid @ModelAttribute("ricetta") Ricetta ricetta, BindingResult bindingResult, Model model) {
-		
-		// Trova il cuoco dal repository utilizzando l'ID fornito
-	    Cuoco currentCuoco = (Cuoco) model.getAttribute("cuoco");	    
 
-	    // Imposta il cuoco nella ricetta
-	    ricetta.setCuoco(currentCuoco);
+
+	@PostMapping("/cuoco/ricetta")
+	public String newRicettaCuoco(@Valid @ModelAttribute("ricetta") Ricetta ricetta, BindingResult bindingResult, @RequestParam("username") String username, Model model) {
+		Credentials tempUser = credentialsRepository.findByUsername(username);
+		User currentUser = tempUser.getUser();
+		Cuoco currentCuoco = this.cuocoRepository.findByNomeAndCognome(currentUser.getNome(), currentUser.getCognome());
+		ricetta.setCuoco(currentCuoco);
+
 		this.ricettaValidator.validate(ricetta, bindingResult);
 		if (!bindingResult.hasErrors()) {
-			this.ricettaRepository.save(ricetta); 
+			this.ricettaRepository.save(ricetta);
 			model.addAttribute("ricetta", ricetta);
 			return "ricetta.html";
 		} else {
-			return "cuoco/formNewRicetta.html"; 
+			return "cuoco/formNewRicetta.html";
 		}
 	}
-	
+
 	@GetMapping(value = "/admin/addCuoco/{idRicetta}")
 	public String addArtist(@PathVariable("idRicetta") Long ricettaId, Model model) {
 		model.addAttribute("cuochi", cuocoService.findAll());
@@ -155,32 +160,34 @@ public class RicettaController {
 		model.addAttribute("ricetta", ricettaRepository.findById(id).get());
 		return "admin/formUpdateRicetta.html";
 	}
-	
+
 	@GetMapping(value = "/cuoco/formUpdateRicetta/{id}/{username}")
-	public String formUpdateRicettaCuoco(@PathVariable("id") Long id, @PathVariable("username") String username, Model model, RedirectAttributes redirectAttributes) {
-	    // Recupera l'utente dal repository
-	    Credentials tempUser = credentialsRepository.findByUsername(username);
-	    User currentUser = tempUser.getUser();
-	    
-	    // Recupera la ricetta dal repository
-	    Ricetta ricetta = ricettaRepository.findById(id).orElse(null);
-	    
-	    // Verifica se il cuoco della ricetta è il cuoco corrente
-	    if (ricetta == null || !ricetta.getCuoco().getNome().equals(currentUser.getNome()) || !ricetta.getCuoco().getCognome().equals(currentUser.getCognome())) {
-	        // Gestisci il caso di accesso non autorizzato
-	        redirectAttributes.addFlashAttribute("messaggioErrore", "Non puoi modificare questa ricetta perché non ti appartiene!");
-	        return "redirect:/cuoco/manageRicette";
-	    }
-	    
-	    // Aggiungi la ricetta al modello e restituisci la vista
-	    model.addAttribute("ricetta", ricetta);
-	    return "cuoco/formUpdateRicetta.html";
+	public String formUpdateRicettaCuoco(@PathVariable("id") Long id, @PathVariable("username") String username,
+			Model model, RedirectAttributes redirectAttributes) {
+		// Recupera l'utente dal repository
+		Credentials tempUser = credentialsRepository.findByUsername(username);
+		User currentUser = tempUser.getUser();
+
+		// Recupera la ricetta dal repository
+		Ricetta ricetta = ricettaRepository.findById(id).orElse(null);
+
+		// Verifica se il cuoco della ricetta è il cuoco corrente
+		if (ricetta == null || !ricetta.getCuoco().getNome().equals(currentUser.getNome())
+				|| !ricetta.getCuoco().getCognome().equals(currentUser.getCognome())) {
+			// Gestisci il caso di accesso non autorizzato
+			redirectAttributes.addFlashAttribute("messaggioErrore",
+					"Non puoi modificare questa ricetta perché non ti appartiene!");
+			return "redirect:/cuoco/manageRicette";
+		}
+
+		// Aggiungi la ricetta al modello e restituisci la vista
+		model.addAttribute("ricetta", ricetta);
+		return "cuoco/formUpdateRicetta.html";
 	}
 
-
-	
 	@GetMapping(value = "/admin/setCuocoToRicetta/{cuocoId}/{ricettaId}")
-	public String setDirectorToMovie(@PathVariable("cuocoId") Long cuocoId, @PathVariable("ricettaId") Long ricettaId, Model model) {
+	public String setDirectorToMovie(@PathVariable("cuocoId") Long cuocoId, @PathVariable("ricettaId") Long ricettaId,
+			Model model) {
 
 		Cuoco cuoco = this.cuocoService.findById(cuocoId);
 		Ricetta ricetta = this.ricettaRepository.findById(ricettaId).get();
@@ -190,9 +197,8 @@ public class RicettaController {
 		model.addAttribute("ricetta", ricetta);
 		return "admin/formUpdateRicetta.html";
 	}
-	
-	
-	/* per aggiungere o togliere ingredienti a mo di lista*/
+
+	/* per aggiungere o togliere ingredienti a mo di lista */
 	@GetMapping("/admin/updateIngredienti/{id}")
 	public String updateIngredienti(@PathVariable("id") Long id, Model model) {
 
@@ -202,7 +208,7 @@ public class RicettaController {
 
 		return "admin/addIngrediente.html";
 	}
-	
+
 	@GetMapping("/cuoco/updateIngredienti/{id}")
 	public String updateIngredientiCuoco(@PathVariable("id") Long id, Model model) {
 
@@ -213,64 +219,68 @@ public class RicettaController {
 		return "cuoco/addIngrediente.html";
 	}
 
-	@GetMapping(value="/admin/addIngredienteToRicetta/{ingredienteId}/{ricettaId}")
-	public String addIngredienteToRicetta(@PathVariable("ingredienteId") Long ingredienteId, @PathVariable("ricettaId") Long ricettaId, Model model) {
+	@GetMapping(value = "/admin/addIngredienteToRicetta/{ingredienteId}/{ricettaId}")
+	public String addIngredienteToRicetta(@PathVariable("ingredienteId") Long ingredienteId,
+			@PathVariable("ricettaId") Long ricettaId, Model model) {
 		Ricetta ricetta = this.ricettaRepository.findById(ricettaId).get();
 		Ingrediente ingrediente = this.ingredienteRepository.findById(ingredienteId).get();
 		Set<Ingrediente> ingredienti = ricetta.getIngredientiUtilizzati();
 		ingredienti.add(ingrediente);
 		this.ricettaRepository.save(ricetta);
-		
+
 		List<Ingrediente> ingredientiToAdd = ingredientiToAdd(ricettaId);
-		
+
 		model.addAttribute("ricetta", ricetta);
 		model.addAttribute("ingredientiToAdd", ingredientiToAdd);
 
 		return "admin/addIngrediente.html";
 	}
-	
-	@GetMapping(value="/cuoco/addIngredienteToRicetta/{ingredienteId}/{ricettaId}")
-	public String addIngredienteToRicettaCuoco(@PathVariable("ingredienteId") Long ingredienteId, @PathVariable("ricettaId") Long ricettaId, Model model) {
+
+	@GetMapping(value = "/cuoco/addIngredienteToRicetta/{ingredienteId}/{ricettaId}")
+	public String addIngredienteToRicettaCuoco(@PathVariable("ingredienteId") Long ingredienteId,
+			@PathVariable("ricettaId") Long ricettaId, Model model) {
 		Ricetta ricetta = this.ricettaRepository.findById(ricettaId).get();
 		Ingrediente ingrediente = this.ingredienteRepository.findById(ingredienteId).get();
 		Set<Ingrediente> ingredienti = ricetta.getIngredientiUtilizzati();
 		ingredienti.add(ingrediente);
 		this.ricettaRepository.save(ricetta);
-		
+
 		List<Ingrediente> ingredientiToAdd = ingredientiToAdd(ricettaId);
-		
+
 		model.addAttribute("ricetta", ricetta);
 		model.addAttribute("ingredientiToAdd", ingredientiToAdd);
 
 		return "cuoco/addIngrediente.html";
 	}
-	
-	@GetMapping(value="/admin/removeIngredienteFromRicetta/{ingredienteId}/{ricettaId}")
-	public String removeIngredienteFromRicetta(@PathVariable("ingredienteId") Long ingredienteId, @PathVariable("ricettaId") Long ricettaId, Model model) {
-		Ricetta ricetta= this.ricettaRepository.findById(ricettaId).get();
+
+	@GetMapping(value = "/admin/removeIngredienteFromRicetta/{ingredienteId}/{ricettaId}")
+	public String removeIngredienteFromRicetta(@PathVariable("ingredienteId") Long ingredienteId,
+			@PathVariable("ricettaId") Long ricettaId, Model model) {
+		Ricetta ricetta = this.ricettaRepository.findById(ricettaId).get();
 		Ingrediente ingrediente = this.ingredienteRepository.findById(ingredienteId).get();
 		Set<Ingrediente> ingredientiUtilizzati = ricetta.getIngredientiUtilizzati();
 		ingredientiUtilizzati.remove(ingrediente);
 		this.ricettaRepository.save(ricetta);
 
 		List<Ingrediente> ingredientiToAdd = ingredientiToAdd(ricettaId);
-		
+
 		model.addAttribute("ricetta", ricetta);
 		model.addAttribute("ingredientiToAdd", ingredientiToAdd);
 
 		return "admin/addIngrediente.html";
 	}
-	
-	@GetMapping(value="/cuoco/removeIngredienteFromRicetta/{ingredienteId}/{ricettaId}")
-	public String removeIngredienteFromRicettaCuoco(@PathVariable("ingredienteId") Long ingredienteId, @PathVariable("ricettaId") Long ricettaId, Model model) {
-		Ricetta ricetta= this.ricettaRepository.findById(ricettaId).get();
+
+	@GetMapping(value = "/cuoco/removeIngredienteFromRicetta/{ingredienteId}/{ricettaId}")
+	public String removeIngredienteFromRicettaCuoco(@PathVariable("ingredienteId") Long ingredienteId,
+			@PathVariable("ricettaId") Long ricettaId, Model model) {
+		Ricetta ricetta = this.ricettaRepository.findById(ricettaId).get();
 		Ingrediente ingrediente = this.ingredienteRepository.findById(ingredienteId).get();
 		Set<Ingrediente> ingredientiUtilizzati = ricetta.getIngredientiUtilizzati();
 		ingredientiUtilizzati.remove(ingrediente);
 		this.ricettaRepository.save(ricetta);
 
 		List<Ingrediente> ingredientiToAdd = ingredientiToAdd(ricettaId);
-		
+
 		model.addAttribute("ricetta", ricetta);
 		model.addAttribute("ingredientiToAdd", ingredientiToAdd);
 
--- siw-food-2/src/main/java/it/uniroma3/siw/model/Ingrediente.java
+++ siw-food-2/src/main/java/it/uniroma3/siw/model/Ingrediente.java
@@ -1,6 +1,6 @@
 package it.uniroma3.siw.model;
 
-import java.util.List;
+
 import java.util.Objects;
 import java.util.Set;
 
@@ -9,7 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinTable;
+
 import jakarta.persistence.ManyToMany;
 
 @Entity
